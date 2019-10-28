4308c046b089ff6c1d380d4489e0ef5875b869ea
Student <example@example.com>
2019-10-28 11:46:43 -0700

    refactor js function with arrays.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,22 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var momInput = $("input#mom").val();
-    var foodInput = $("input#food").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
+    var blanks = ["person1", "mom", "food", "verb", "noun"];
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    });
 
-    $(".person1").text (person1Input);
-    $(".mom").text (momInput);
-    $(".food").text (foodInput);
-    $(".verb").text(verbInput);
-    $(".noun").text (nounInput);
+    // var person1Input = $("input#person1").val();
+    // var momInput = $("input#mom").val();
+    // var foodInput = $("input#food").val();
+    // var verbInput = $("input#verb").val();
+    // var nounInput = $("input#noun").val();
+    //
+    // $(".person1").text (person1Input);
+    // $(".mom").text (momInput);
+    // $(".food").text (foodInput);
+    // $(".verb").text(verbInput);
+    // $(".noun").text (nounInput);
 
     $("#story").show();
 
